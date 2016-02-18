37656a872cf37b10ab324125ae2676abd76cc1a5
Nguyen The Thuc <thucfami@gmail.com>
2016-02-18 22:52:55 +0700

    Email

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -10,12 +10,19 @@ return [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            'viewPath' => '@common/mail',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'viewPath' => '@app/mail',
+            'useFileTransport' => false,//set this property to false to send mails to real email addresses
+            //comment the following array to send mail using php's mail function
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'h2016@gmail.com',
+                'password' => 'lr3fgkRh5',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
+
         'Urlseo' => [
                     'class' => 'common\seo\Urlseo'
         ],
