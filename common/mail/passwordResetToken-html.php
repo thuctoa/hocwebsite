4ce19c39f203c82745e665d5f999eae4e0bf9128
Nguyen The Thuc <thucfami@gmail.com>
2016-02-19 14:42:39 +0700

    Mail đổi mật khẩu

--- common/mail/passwordResetToken-html.php
+++ common/mail/passwordResetToken-html.php
@@ -7,7 +7,9 @@ use yii\helpers\Html;
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
 <div class="password-reset">
-    <p>Xin chào <?= Html::encode($user->username) ?>,</p>
+    <p>Xin chào <?= Html::encode($user->displayname).
+        ' bạn có tài khoản là '
+        .Html::encode($user->username) ?>,</p>
 
     <p>Bạn hãy click vào đường link phía dưới để thay đổi mật khẩu của bạn:</p>
 
--- common/mail/passwordResetToken-text.php
+++ common/mail/passwordResetToken-text.php
@@ -5,7 +5,7 @@
 
 $resetLink = Yii::$app->urlManager->createAbsoluteUrl(['site/reset-password', 'token' => $user->password_reset_token]);
 ?>
-Xin chào <?= $user->username ?>,
+Xin chào <?= $user->displayname.' bạn có tài khoản là '.$user->username ?>,
 
 Bạn hãy click vào đường link phía dưới để thay đổi mật khẩu:
 
