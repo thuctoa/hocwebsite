b75c875c339e276d50983bbcafd68c0f5482db10
Nguyen The Thuc <thucfami@gmail.com>
2016-02-19 14:47:49 +0700

    Họ và tên

--- frontend/views/site/updateUser.php
+++ frontend/views/site/updateUser.php
@@ -1,8 +1,9 @@
 <div class="thaydoithongtin">
     <div class="thaydoithongtinform">
     <form action="/site/update-user" method="post">
-        <label class="control-label" >Họ và tên </label> 
+        <label class="control-label" >Họ</label> 
         <input  class="form-control" type="text" name="user[first_name]" value="<?=$model['first_name']?>"><br>
+        <label class="control-label" >Tên</label>
         <input  class="form-control" type="text" name="user[last_name]" value="<?=$model['last_name']?>"><br>
         <label class="control-label" > Số điện thoại </label> 
         <input class="form-control" type="text" name="user[phone_number]" value="<?=$model['phone_number']?>"><br>
