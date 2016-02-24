6cf7873e6d1b8987436163fea3ad5d419ddc4854
Nguyen The Thuc <thucfami@gmail.com>
2016-02-24 21:59:28 +0700

    nen gzip

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,4 +1,8 @@
 <?php
+    if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
+        ob_start("ob_gzhandler");
+    else
+        ob_start(); 
 
 /* @var $this \yii\web\View */
 /* @var $content string */
