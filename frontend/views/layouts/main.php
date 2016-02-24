0e7df24378ea90890bae2cd5b4a3f53323c79280
Nguyen The Thuc <thucfami@gmail.com>
2016-02-24 22:01:52 +0700

    nen gzip

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -1,9 +1,22 @@
 <?php
-    if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
-        ob_start("ob_gzhandler");
-    else
-        ob_start(); 
-
+     // initialize ob_gzhandler function to send and compress data
+   ob_start ("ob_gzhandler");
+ 
+   // send the requisite header information and character set
+   header ("content-type: text/css; charset: UTF-8");
+ 
+   // check cached credentials and reprocess accordingly
+   header ("cache-control: must-revalidate");
+ 
+   // set variable for duration of cached content
+   $offset = 60 * 60;
+ 
+   // set variable specifying format of expiration header
+   $expire = "expires: " . gmdate ("D, d M Y H:i:s", time() + $offset) . " GMT";
+ 
+   // send cache expiration header to the client broswer
+   header ($expire);
+   
 /* @var $this \yii\web\View */
 /* @var $content string */
 
