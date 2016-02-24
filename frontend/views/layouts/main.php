5a3a076d9fd14bda932e84495c3f82d3eb97027a
Nguyen The Thuc <thucfami@gmail.com>
2016-02-24 22:12:14 +0700

    nen gzip

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -14,14 +14,7 @@ AppAsset::register($this);
 <?php 
     $this->beginPage() 
 ?>
-<?php
-   ob_start ("ob_gzhandler");
-   header ("content-type: text/css; charset: UTF-8");
-   header ("cache-control: must-revalidate");
-   $offset = 48 * 60 * 60;
-   $expire = "expires: " . gmdate ("D, d M Y H:i:s", time() + $offset) . " GMT";
-   header ($expire);
-?>
+
 <?php
 if (substr_count($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip'))
 	ob_start("ob_gzhandler");
