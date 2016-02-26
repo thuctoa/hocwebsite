6980b9642ce916b68901f1c48b987876e803c437
Nguyen The Thuc <thucfami@gmail.com>
2016-02-26 23:46:45 +0700

    xac minh bing

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -16,6 +16,7 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
+    <meta name="msvalidate.01" content="B693FEE86D66E8587C4E1AA37056D47E" />
     <meta name="description" content="Hướng dẫn lập trình học website mã nguồn mở PHP">
     <meta name="keywords" content="Học website, học lập trình php, thiết kế website, 
           khóa học lập trình php miễn phí">
