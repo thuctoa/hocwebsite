6c1710cf63586e4be9bb41065408dd73c611f5e2
Nguyen The Thuc <thucfami@gmail.com>
2016-02-27 13:15:52 +0700

    ben nen js

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -17,6 +17,26 @@ return [
     'controllerNamespace' => 'frontend\controllers',
     'homeUrl' => '/',
     'components' => [
+        'assetManager' => [
+            'class' => 'yii\web\AssetManager',
+            'bundles' => [
+                        'yii\web\JqueryAsset' => [
+                            'js' => [
+                                YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+                            ]
+                        ],
+                        'yii\bootstrap\BootstrapAsset' => [
+                            'css' => [
+                                YII_ENV_DEV ? 'css/bootstrap.css' :         'css/bootstrap.min.css',
+                            ]
+                        ],
+                        'yii\bootstrap\BootstrapPluginAsset' => [
+                            'js' => [
+                                YII_ENV_DEV ? 'js/bootstrap.js' : 'js/bootstrap.min.js',
+                            ]
+                        ]
+            ],
+        ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
