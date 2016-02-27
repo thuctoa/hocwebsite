ae18fda53b0b2076a1adca5ec15dae829765ac79
Nguyen The Thuc <thucfami@gmail.com>
2016-02-27 13:18:20 +0700

    ben nen js

--- frontend/config/main.php
+++ frontend/config/main.php
@@ -22,17 +22,17 @@ return [
             'bundles' => [
                         'yii\web\JqueryAsset' => [
                             'js' => [
-                                YII_ENV_DEV ? 'jquery.js' : 'jquery.min.js'
+                                YII_ENV_DEV ? 'jquery.min.js' : 'jquery.min.js'
                             ]
                         ],
                         'yii\bootstrap\BootstrapAsset' => [
                             'css' => [
-                                YII_ENV_DEV ? 'css/bootstrap.css' :         'css/bootstrap.min.css',
+                                YII_ENV_DEV ? 'css/bootstrap.min.css' :         'css/bootstrap.min.css',
                             ]
                         ],
                         'yii\bootstrap\BootstrapPluginAsset' => [
                             'js' => [
-                                YII_ENV_DEV ? 'js/bootstrap.js' : 'js/bootstrap.min.js',
+                                YII_ENV_DEV ? 'js/bootstrap.min.js' : 'js/bootstrap.min.js',
                             ]
                         ]
             ],
