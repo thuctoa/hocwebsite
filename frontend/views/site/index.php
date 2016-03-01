3d50bcf374e6bf3b07072afd27ae20e836063dd5
Nguyen The Thuc <thucfami@gmail.com>
2016-03-01 11:40:41 +0700

    toi uu facebook o bai viet

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -14,6 +14,25 @@ $this->title = Yii::t('app','Học Website');
         $id=$baiviet['id'];
         $this->title = $baiviet['title'];
     ?>
+    <div id="fb-root"></div>
+    <script>
+         window.fbAsyncInit = function() {
+        FB.init({
+          appId      : '213920512290093',
+          xfbml      : true,
+          version    : 'v2.5'
+        });
+      };
+
+      (function(d, s, id){
+         var js, fjs = d.getElementsByTagName(s)[0];
+         if (d.getElementById(id)) {return;}
+         js = d.createElement(s); js.id = id;
+         js.src = "//connect.facebook.net/vi_VN/sdk.js";
+         fjs.parentNode.insertBefore(js, fjs);
+       }(document, 'script', 'facebook-jssdk'));
+
+    </script>
         <div class="col-md-8">
             <div class="noidung"> 
                 <div class="modau">
