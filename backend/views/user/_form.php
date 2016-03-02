537162a8fbce425746ce1a49438d80d68c810e13
Nguyen The Thuc <thucfami@gmail.com>
2016-03-02 21:37:14 +0700

    kiem tra gui bai

--- backend/views/user/_form.php
+++ backend/views/user/_form.php
@@ -12,17 +12,8 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'first_name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'last_name')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
-
-    <?= $form->field($model, 'phone_number')->textInput(['maxlength' => true]) ?>
-
-    <?php echo $form->field($model, 'guibai')->dropDownList(['0' => 'Chưa gửi', '1' => 'Đã gửi']); ?>
+    
+    <?php echo $form->field($model, 'guibai')->dropDownList(['0' => 'Chưa được xử lý', '1' => 'Đã xử lý']); ?>
     
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
